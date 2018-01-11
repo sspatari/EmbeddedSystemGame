f6b26cca1327d1a7a57b8fe39b31e3d938d9f9c0
sspatari <spatari.stanislav@gmail.com>
2018-01-11 14:33:45 +0200

    Initiat project with simple program

---
+++ Debug/src/helloworld.d
@@ -0,0 +1,48 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/sleep.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/sleep.h:
---
+++ Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../getting_started_with_ZYBO_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
